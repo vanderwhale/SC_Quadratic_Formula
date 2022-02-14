d781f81bf8195f7de9e5a6ff7d583c2e0fe558af
Sam <97985983+vanderwhale@users.noreply.github.com>
2022-02-14 17:41:50 -0500

    Quad Formula Program v1.0

--- quadratic_formula.py
+++ quadratic_formula.py
@@ -1,14 +1,30 @@
-#!/usr/bin/pyhton3
-print('Solve Quadratic Equation:')
-
 import math
 import cmath
-def quad_solve(a,b,c):
-    D = b*b - 4*a*c
-    SD = D**0.5
 
-    if (D>0):
-        x1 = (-b + SD / (2*a)
-        x2 = (2*c) / (-b+SD)
-    return x1, x2
-print(quad_solve(1,1e10,4))
+def quad_solve(a,b,c):
+
+    D = b*b - 4*a*c
+
+    if D>0:
+        SD = D**0.5;
+        if b > 0: SD = -SD
+        print((-b + SD) / (2*a))
+        print((2*c) / (-b + SD))
+
+    elif D == 0:
+        print(-b / (2*a))
+        print(-b / (2*a))
+
+    else:
+        print((-b - cmath.sqrt(D)) / (2*a))
+        print((-b + cmath.sqrt(D)) / (2*a))
+
+a = float(input('a='))
+b = float(input('b='))
+c = float(input('c='))
+
+if a == 0:
+    print('idiot')
+
+else:
+    quad_solve(a,b,c)
