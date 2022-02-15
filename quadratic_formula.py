70ad7518bdd36da19b92566306b84c4bc8b31b23
Sam <97985983+vanderwhale@users.noreply.github.com>
2022-02-15 13:16:26 -0500

    account for c=0

--- quadratic_formula.py
+++ quadratic_formula.py
@@ -6,16 +6,23 @@ def quad_solve(a,b,c):
     #Discriminant
     D = b*b - 4*a*c
 
-    if D>0:
-        SD = D**0.5;
+    #If c = 0 -- One Real and One Zero Root
+    if c == 0:
+        print((-b+b) / (2*a))
+        print((-b-b) / (2*a))
+
+    #Positive Discriminant -- Real and Distinct Roots
+    elif D > 0:
+        SD = D**0.5; #Square Root of the Discriminant
         print((2*c) / (-b - SD))
         print((2*c) / (-b + SD))
 
+    #Discriminant is Zero -- Real and Equal Roots
     elif D == 0:
         print(-b / (2*a))
         print(-b / (2*a))
 
-    #Complex roots
+    #Discriminant is Negative -- Complex and Distinct Roots
     else:
         print((-b - cmath.sqrt(D)) / (2*a))
         print((-b + cmath.sqrt(D)) / (2*a))
@@ -25,7 +32,7 @@ b = float(input('b='))
 c = float(input('c='))
 
 if a == 0:
-    print('idiot')
+    print('Not a Quadratic')
 
 else:
     quad_solve(a,b,c)
